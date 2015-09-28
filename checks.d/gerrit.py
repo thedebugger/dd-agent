01abd4b9d3c59430cca148d89804c69cef26fd74
Rohitasva <rohitasva@users.noreply.github.com>
2015-09-28 12:17:46 -0700

    Update gerrit.py

--- checks.d/gerrit.py
+++ checks.d/gerrit.py
@@ -6,14 +6,14 @@ import simplejson as json
 class Gerrit(AgentCheck):
     def check(self, instance):
         if 'gerrit_status_url' not in instance:
-            raise Exception('Hydra instance missing "gerrit_status_url" value.')
-        base_url = instance.get('hydra_status_url')
+            raise Exception('Gerrit instance missing "gerrit_status_url" value.')
+        base_url = instance.get('gerrit_status_url')
 
         projects_instance = instance
         projects_instance['gerrit_status_url'] = base_url + "/projects/"
 
     def _get_data(self, instance):
-        url = instance.get('hydra_status_url')
+        url = instance.get('gerrit_status_url')
 
         auth = None
         if 'user' in instance and 'password' in instance:
@@ -21,10 +21,10 @@ class Gerrit(AgentCheck):
 
         # Submit a service check for status page availability.
         parsed_url = urlparse.urlparse(url)
-        hydra_host = parsed_url.hostname
-        hydra_port = parsed_url.port or 80
-        service_check_name = 'hydra.can_connect'
-        service_check_tags = ['host:%s' % hydra_host, 'port:%s' % hydra_port]
+        gerrit_host = parsed_url.hostname
+        gerrit_port = parsed_url.port or 80
+        service_check_name = 'gerrit.can_connect'
+        service_check_tags = ['host:%s' % gerrit_host, 'port:%s' % gerrit_port]
         try:
             headers = {'Accept': 'application/json'}
             r = requests.get(url, auth=auth, headers=headers)
@@ -40,4 +40,3 @@ class Gerrit(AgentCheck):
         body = r.content
         resp_headers = r.headers
         return body, resp_headers.get('content-type', 'text/plain')
-       
